be84133817da9e826ae04d8cce08016c29e74ae2
gnari0 <colbyschneider@mines.edu>
2022-06-06 13:45:26 -0600

    updated imageOverlay

--- ImageIN/imageOverlay.py
+++ ImageIN/imageOverlay.py
@@ -1,17 +1,25 @@
 from PIL import Image
 
-def renderOverlay(slides):
-    background = Image.open('image1.png')
+def renderOverlay(slides, transparency):
+    #open first image, convert to RGBA, set transparency
+    background = slides[0]
+    background = background.convert("RGBA")
+    background.putalpha(transparency[i])
+    
     x = background.size[0]//2
     y = background.size[1]//2
-    background = Image.alpha_composite(
-        Image.new("RGBA", background.size),
-        background.convert('RGBA')
-    )
+    
+    #background = Image.alpha_composite(
+    #    Image.new("RGBA", background.size),
+    #    background.convert('RGBA'))
+    
+    #convert to RGBA, set transparency, and paste consecutive images over background
+    i = 1
     for element in slides:
+        element.putalpha(transparency[i])
         background.paste(
             element,
             (x, y),
-            element
-        )
+            element)
+        i += 1
     background.show()
