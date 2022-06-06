66576ac463e3db41743fe24ed2200594205edfd7
gnari0 <colbyschneider@mines.edu>
2022-06-06 15:08:34 -0600

    imageOverlay no background tiles removed

--- ImageIN/imageOverlay.py
+++ ImageIN/imageOverlay.py
@@ -12,20 +12,19 @@ def renderOverlay(slides, transparency):
     i = 0
     for element in slides:
         if (i > 0):
-            print("paste ", i)
             x = (background.width - element.width)//2
             y = (background.height - element.height)//2
-            element = element.convert("RGBA")
-            element.putalpha(transparency[i])
+            mask = element.convert("RGBA")
+            mask.putalpha(transparency[i])
             background.paste(
                 element,
                 (x, y),
-                element)
+                element1)
         i += 1
     background.save("composite.png", format="PNG")
     background.show()
 
 #TEST CODE
-#img1 = Image.open("Images\L15 XPL.png")
-#img2 = Image.open("Images\L15-Plain.jpg")
-#renderOverlay([img1, img2], [255, 128])
+img1 = Image.open("Images\L15 XPL.png")
+img2 = Image.open("Images\L15-Plain.jpg")
+renderOverlay([img1, img2], [255, 128])
