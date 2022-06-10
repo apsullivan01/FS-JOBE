428ee25c39301eef039a78f3298f9b3e528c85c4
jwandasan <jtwandasan@gmail.com>
2022-06-09 18:40:19 -0600

    Reformated the user interface code, made it so that the enter key acts as submit and the text input section is now only a single line instead of multiline.

--- userInterface.py
+++ userInterface.py
@@ -24,11 +24,15 @@ from EditImg import EditImg
 # Basic GUI Implementation
 class WidgetContainer(BoxLayout):
     def __init__(self, **kwargs):
+        # Call to box layout constructor
         super(WidgetContainer, self).__init__(**kwargs)
-        self.input_num = None
+
+        # Declaring options for spacing and look
         self.orientation = 'vertical'
         self.row_default_height = 20
         self.spacing = 10
+
+        # Instancing UI and EditImg
         self.sliders = UI()
         self.image_edit = EditImg()
 
@@ -38,68 +42,87 @@ class WidgetContainer(BoxLayout):
         to_sliders = Button(text='To Sliders', on_release=self.to_sliders)
         edit_button = Button(text='Edit Images', on_release=self.edit_images)
 
+        # Adding buttons to the home screen
         self.add_widget(select_images)
         self.add_widget(edit_button)
         self.add_widget(to_sliders)
 
-    # Go back to home state
+    # Go back to "home" state
     def go_home(self, event):
+        # Clears existing widgets to simulate change to Home screen
         self.clear_widgets()
+
         # Each button corresponds to the action they would like to take
         select_images = Button(text='Select Images', on_release=self.select_images)
         to_sliders = Button(text='To Sliders', on_release=self.to_sliders)
         edit_button = Button(text='Edit Images', on_release=self.edit_images)
 
+        # Adding buttons to the "new" home screen
         self.add_widget(select_images)
         self.add_widget(edit_button)
         self.add_widget(to_sliders)
 
     # Clear the interface and setup image selection
     def select_images(self, event):
+        # Clears existing widgets to simulate change to Select Images screen
         self.clear_widgets()
-        back_button = Button(text='Home', size_hint=(1, .5), on_release=self.go_home)
-        self.add_widget(back_button)
 
-        input_label = Label(text='Enter number of images to be imported in text field below (max 10)')
+        # Creating necessary widgets - home button, label(for directions), and input
+        back_button = Button(text='Home', size_hint=(1, .5), on_release=self.go_home)
+        self.input_num = TextInput(text='', size_hint=(1, .2), write_tab=False, multiline=False,
+                                   on_text_validate=self.submit)
+        input_label = Label(text='Enter number of images to be imported in text field below (max 10) \n'
+                                 'Only use files in the .png format')
+
+        self.add_widget(back_button)
         self.add_widget(input_label)
-        self.input_num = TextInput(text='', size_hint=(1, .2))
-        submit_button = Button(text='Submit', on_release=self.submit, size_hint=(1, .2))
         self.add_widget(self.input_num)
-        self.add_widget(submit_button)
 
     # Clear the interface and bring up loadable states
     def edit_images(self, event):
-        # Home Button
+        # Clears existing widgets to simulate change to Edit Images screen
         self.clear_widgets()
-        back_button = Button(text='Home', on_release=self.go_home)
-        self.add_widget(back_button)
 
+        # Creates the entirety of the slider and image sections
+        self.image_edit.showImageObj(self.imageList)
+
+        # Creating back button and new box layout for images and sliders
+        back_button = Button(text='Home', size_hint=(1, 0.1), on_release=self.go_home)
         box = BoxLayout(orientation='vertical')
+
+        # Adding created widgets to the user interface
+        box.add_widget(back_button)
         box.add_widget(self.image_edit)
         self.add_widget(box)
 
-        #self.numImg  = self.input_num.text
-        #self.imageListClass = imageIn.imgImport(int(self.numImg))
-        #self.imageList = self.imageListClass.getImageList()
-        self.image_edit.showImageObj(self.imageList)
+
 
     # Go to sliders for image stacking and previewing
     def to_sliders(self, event):
+        # Clears existing widgets to simulate change to Sliders screen
         self.clear_widgets()
 
-        box = BoxLayout(orientation='vertical')
+        # Creating back button and new box layout for images and sliders
         back_button = Button(text='Home', size_hint=(1, 0.1), on_release=self.go_home)
+        box = BoxLayout(orientation='vertical')
 
+        # Adding created widgets to the user Interface
         box.add_widget(back_button)
-
         box.add_widget(self.sliders)
         self.add_widget(box)
 
 
     def submit(self, obj):
+        # Grabs the number of images in the input_num TextInput
         self.numImg = self.input_num.text
+
+        # Brings up the image importing interface
         self.imageListClass = imageIn.imgImport(int(self.numImg))
+
+        # Gets the list of images imported
         self.imageList = self.imageListClass.getImageList()
+
+        # Creates the entirety of the slider and image sections
         self.sliders.showImageObj(self.imageList)
 
 
