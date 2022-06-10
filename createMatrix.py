0201282e57b06a780023d3ee31521a0c52b9802a
apsullivan01 <apsullivan@mines.edu>
2022-06-10 10:24:17 -0600

    Fixed Matrix Class

--- createMatrix.py
+++ createMatrix.py
@@ -4,10 +4,6 @@ import numpy as np
 from PIL import Image
 import imageio
 
-matrixList = []
-pokeThroughList = []
-
-#this all kind of works, the instancing is fucked tbh
 #unsure how all of this will be called
 #but basic idea is as follows:
     #create new image matrix, this loops through all layers and creates 3d matrix of the individual pixel values
@@ -15,23 +11,36 @@ pokeThroughList = []
         #pokeThrough will create a list of each layers rgba value (as a touple) at that point
 
 class pokeThrough:
-    pokeThroughList = []
-    def pokeThrough(x, y):
-        for layer in matrixList:
-            rgba = (layer[x,y,0], layer[x,y,1], layer[x,y,2], layer[x,y,3])
-            pokeThroughList.append(rgba)
-
-class imgMatrix:
-    def createMatrix(imgList):
+    def __init__(self, imgList, x, y):
+        self.x = x
+        self.y = y
+        self.imgList = imgList
+        pokeThroughList = []
+        matrixList = []
+        self.createMatrix(imgList)
+    def createMatrix(self, imgList):
         #loop through all images
         for image in imgList:
             img = Image.open(image)
             #cast to 3d array
             arr = np.array(img)
-            matrixList.append(arr)
+            self.matrixList.append(arr)
+            self.pokeThrough(self.x,self.y)
             #do something with said array
             #needs to be updated
-            np.savetxt("test.csv", arr, delimiter=",")
+            #np.savetxt("test.csv", arr, delimiter=",")
+    def pokeThrough(self, x, y):
+        for layer in self.matrixList:
+            rgba = (layer[x,y,0], layer[x,y,1], layer[x,y,2], layer[x,y,3])
+            self.pokeThroughList.append(rgba)
+            return self.pokeThroughList
+    
+    
+    
+    
+
+
+    
     
 
             
