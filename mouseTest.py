174a8839bee628cf8be5fe57c0e06e2d1672e9b5
apsullivan01 <apsullivan@mines.edu>
2022-06-10 12:42:12 -0600

    Test

--- mouseTest.py
+++ mouseTest.py
@@ -30,7 +30,7 @@ class UserInterface(App):
     # Build starting interface
     def build(self):
         widgetContainer = MoveMe()
-        MoveMe.add_widget(Image(source = "/Images/L03.png"))
+        widgetContainer.add_widget(Image(source = 'Images/L03.png', size = (dp(300),dp(300))))
         return widgetContainer
 
 
