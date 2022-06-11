27806ebcb9e416583e5199e238b5f15e6bdc3fef
gnari0 <colbyschneider@mines.edu>
2022-06-11 15:56:24 -0600

    changed repositioning

--- imgAlign.py
+++ imgAlign.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from matplotlib import offsetbox
 
 #takes list of images and corresponding lists of transformation matrices, as well as bool for saving images
 
@@ -8,8 +9,6 @@ def imgAlign(dict, bgName):
     size = Image.open(bgName).size
     for key, value in dict.items():
         element = Image.open(key)
-        #translation
-        element = element.transform(element.size, Image.AFFINE, (1, 0, value[0][0], 0, 1, value[0][1]))
         #rotation
         element = element.rotate(value[1])
         #resizing
@@ -18,7 +17,10 @@ def imgAlign(dict, bgName):
         new_size = int(x * value[2]), int(y * value[2])
         print(new_size)
         element = element.resize(new_size)
-        element = element.crop((0, 0, size[0], size[1]))
+
+        element = element.crop((value[0][0] * -1, value[0][1] * -1, size[0] - value[0][0], size[1] - value[0][1]))
+        #translation
+        #element = element.transform(element.size, Image.AFFINE, (1, 0, value[0][0], 0, 1, value[0][1]))
 
         element.save(key + "_aligned.png", format="PNG")
         list.append(key + "_aligned.png")
